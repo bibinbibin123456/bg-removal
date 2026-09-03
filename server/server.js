65104e43c2c5228f2d9efd2ba1b44c2d4ccbc5d2
bibinbibin123456 <bibinpbabu12@gmailcom>
2026-09-03 14:04:16 +0530

    Update server configuration

--- server/server.js
+++ server/server.js
@@ -12,31 +12,12 @@ const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-// Allowed frontend URLs
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174",
-  "https://bg-removal-frontend-liart.vercel.app",
-];
+
 
 // CORS
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests without origin
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      // Allow known frontend URLs
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin:"https://bg-removal-frontend-liart.vercel.app/cd",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "token"],
